6afdc08be988c7b1e8eeaa270522351aed01cb6b
d521bb85 <d521bb85@gmail.com>
2022-07-26 18:24:42 +0300

    WIP: Implement the param to display subcategories only in specific categories

--- javascripts/discourse/components/subcategory-list.js
+++ javascripts/discourse/components/subcategory-list.js
@@ -9,12 +9,12 @@ export default class SubcategoryList extends GlimmerComponent {
     super(...arguments);
 
     const parent = getOwner(this).lookup("controller:navigation/category");
-    if (parent && parent.showingParentCategory && this.#shouldDisplay(parent)) {
+    if (parent && parent.showingParentCategory && this.shouldDisplay(parent)) {
       this.parentCategory = parent;
     }
   }
 
-  #shouldDisplay(parent) {
+  shouldDisplay(parent) {
     const displayInCategories = this.args?.params?.displayInCategories
       .split(",")
       .map(Number);
