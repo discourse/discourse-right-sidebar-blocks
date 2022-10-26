6c12ef19bff19589ed3e00c267f42e4f7da37b04
Arpit Jalan <er.ajalan@gmail.com>
2022-10-26 18:48:40 +0530

    If displayInCategories is specified then show block in those categories only

--- javascripts/discourse/components/popular-tags.js
+++ javascripts/discourse/components/popular-tags.js
@@ -29,8 +29,6 @@ export default class PopularTags extends Component {
 
     if (this.topTags.length === 0) {
       return false;
-    } else if (!category) {
-      return true;
     } else if (!this.shouldDisplay(category.id)) {
       return false;
     } else {
