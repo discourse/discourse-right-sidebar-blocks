b76d773e3778478a498405c223c805bc622749cf
Carson Chang <carsick@yahoo.com>
2022-12-23 13:13:18 -0800

    added top topics, tag topics

--- javascripts/discourse/components/tag-topics.js
+++ javascripts/discourse/components/tag-topics.js
@@ -3,26 +3,27 @@ import Component from "@glimmer/component";
 import { ajax } from "discourse/lib/ajax";
 import { tracked } from "@glimmer/tracking";
 
-export default class TopTopics extends Component {
-  @tracked toptopics = null;
+export default class TagTopics extends Component {
+  @tracked tagtopics = null;
 
   constructor() {
     super(...arguments);
     const count = this.args?.params?.count || 5;
-    const period = this.args?.params?.period || "weekly";
-    const topjson = "/top.json?period=" + period.toString();
+    const tagtopics = "/tag/" + this.args?.params?.tag + ".json";
 
-    ajax(topjson).then((data) => {
+    this.blockTitle = this.args?.params?.title || "#" + this.args?.params?.tag;
+
+    ajax(tagtopics).then((data) => {
       let results = [];
       results = data.topic_list.topics;
-      this.toptopics = results.slice(0, count);
-      this.toptopics.forEach((topic) => {
+      this.tagtopics = results.slice(0, count);
+      this.tagtopics.forEach((topic) => {
         topic["category"] = Category.findById(topic.category_id);
       });
     });
   }
 
   willDestroy() {
-    this.toptopics = null;
+    this.tagtopics = null;
   }
 }
