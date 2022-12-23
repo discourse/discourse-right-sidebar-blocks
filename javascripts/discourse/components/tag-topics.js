d8a32a0fec2d0917b62d51485e16cb6dc2f03fa7
Carson Chang <carsick@yahoo.com>
2022-12-23 15:42:55 -0800

    scss code style, renamed js variables

--- javascripts/discourse/components/tag-topics.js
+++ javascripts/discourse/components/tag-topics.js
@@ -4,26 +4,26 @@ import { ajax } from "discourse/lib/ajax";
 import { tracked } from "@glimmer/tracking";
 
 export default class TagTopics extends Component {
-  @tracked tagtopics = null;
+  @tracked tagTopics = null;
 
   constructor() {
     super(...arguments);
     const count = this.args?.params?.count || 5;
-    const tagtopics = "/tag/" + this.args?.params?.tag + ".json";
+    const tagTopicsUrl = "/tag/" + this.args?.params?.tag + ".json";
 
     this.blockTitle = this.args?.params?.title || "#" + this.args?.params?.tag;
 
-    ajax(tagtopics).then((data) => {
+    ajax(tagTopicsUrl).then((data) => {
       let results = [];
       results = data.topic_list.topics;
-      this.tagtopics = results.slice(0, count);
-      this.tagtopics.forEach((topic) => {
+      this.tagTopics = results.slice(0, count);
+      this.tagTopics.forEach((topic) => {
         topic["category"] = Category.findById(topic.category_id);
       });
     });
   }
 
   willDestroy() {
-    this.tagtopics = null;
+    this.tagTopics = null;
   }
 }
--- javascripts/discourse/components/top-topics.js
+++ javascripts/discourse/components/top-topics.js
@@ -4,25 +4,25 @@ import { ajax } from "discourse/lib/ajax";
 import { tracked } from "@glimmer/tracking";
 
 export default class TopTopics extends Component {
-  @tracked toptopics = null;
+  @tracked topTopics = null;
 
   constructor() {
     super(...arguments);
     const count = this.args?.params?.count || 5;
     const period = this.args?.params?.period || "weekly";
-    const topjson = "/top.json?period=" + period.toString();
+    const topTopicsUrl = "/top.json?period=" + period.toString();
 
-    ajax(topjson).then((data) => {
+    ajax(topTopicsUrl).then((data) => {
       let results = [];
       results = data.topic_list.topics;
-      this.toptopics = results.slice(0, count);
-      this.toptopics.forEach((topic) => {
+      this.topTopics = results.slice(0, count);
+      this.topTopics.forEach((topic) => {
         topic["category"] = Category.findById(topic.category_id);
       });
     });
   }
 
   willDestroy() {
-    this.toptopics = null;
+    this.topTopics = null;
   }
 }
