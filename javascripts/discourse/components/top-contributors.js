ff29074c39a20740cb3197bd01bc555396e294b5
Jean Perez <jmperez127@gmail.com>
2023-07-18 19:01:50 -0400

    FEATURE: sort by likes given and add groups exclusion

--- javascripts/discourse/components/top-contributors.js
+++ javascripts/discourse/components/top-contributors.js
@@ -4,17 +4,23 @@ import { tracked } from "@glimmer/tracking";
 
 export default class TopContributors extends Component {
   @tracked topContributors = null;
+  order = null;
+  viewAllUrl = null;
 
   constructor() {
     super(...arguments);
-
     const count = this.args?.params?.count || 5;
+    this.order = this.args?.params?.order || "likes_received";
+    const period = this.args?.params?.period || "yearly";
 
-    ajax(`/directory_items.json?period=yearly&order=likes_received`).then(
-      (data) => {
-        this.topContributors = data.directory_items.slice(0, count);
-      }
-    );
+    const excludedGroupNames = this.args?.params?.excludedGroupNames || "";
+
+    this.viewAllUrl = `/u?order=${this.order}&period=${period}`;
+    const requestURL = `/directory_items.json?period=${period}&order=${this.order}&exclude_groups=${excludedGroupNames}&limit=${count}`;
+
+    ajax(requestURL).then((data) => {
+      this.topContributors = data.directory_items.slice(0, count);
+    });
   }
 
   willDestroy() {
--- test/acceptance/top-contributors-test.js
+++ test/acceptance/top-contributors-test.js
@@ -87,3 +87,75 @@ acceptance("Right Sidebar - Top Contributors - Custom count", function (needs) {
     );
   });
 });
+
+acceptance(
+  "Right Sidebar - Top Contributors - Custom order, group exclusion and period",
+  function (needs) {
+    const blocksJSON = [
+      {
+        name: "top-contributors",
+        params: [
+          {
+            name: "count",
+            value: "3",
+          },
+          {
+            name: "order",
+            value: "likes_given",
+          },
+          {
+            name: "period",
+            value: "weekly",
+          },
+          {
+            name: "excludedGroupNames",
+            value: "Team|contributors",
+          },
+        ],
+      },
+    ];
+
+    needs.hooks.beforeEach(() => {
+      settings.blocks = JSON.stringify(blocksJSON);
+    });
+
+    needs.hooks.afterEach(() => {
+      settings.blocks = "[]";
+    });
+
+    needs.pretender((server, helper) => {
+      server.get("/directory_items.json", (request) => {
+        if (
+          request.queryParams.period === "weekly" &&
+          request.queryParams.order === "likes_given" &&
+          request.queryParams.exclude_groups === "Team|contributors" &&
+          request.queryParams.limit === "3"
+        ) {
+          return helper.response(DirectoryFixtures["directory_items"]);
+        }
+
+        return helper.response(400, {}, { error: "Bad Request" });
+      });
+    });
+
+    test("Viewing latest", async function (assert) {
+      await visit("/");
+
+      assert.ok(visible(".tc-right-sidebar"), "sidebar element is present");
+      assert.ok(
+        visible(".top-contributors--container"),
+        "top contributors element is present"
+      );
+
+      assert.strictEqual(
+        queryAll(".top-contributors--user").length,
+        3,
+        "custom limit is respected"
+      );
+
+      assert
+        .dom(".top-contributors--view-all")
+        .hasAttribute("href", "/u?order=likes_given&period=weekly");
+    });
+  }
+);
