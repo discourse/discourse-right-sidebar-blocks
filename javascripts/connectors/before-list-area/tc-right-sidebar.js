76e0463463fde19b3af89897aaa6a2902590d1e6
brrusselburg <25828824+brrusselburg@users.noreply.github.com>
2024-08-07 13:28:02 -0500

    merge functions into one

--- javascripts/connectors/before-list-area/tc-right-sidebar.js
+++ javascripts/connectors/before-list-area/tc-right-sidebar.js
@@ -6,39 +6,13 @@ export default {
     this.reopen({
       router: service(),
 
-      // @discourseComputed(
-      //   "router.currentRouteName",
-      //   "router.currentRoute.attributes.category.slug",
-      //   "router.currentRoute.attributes.tag.id"
-      // )
-      // showSidebar(currentRouteName, categorySlug, tagId) {
-      //   if (this.site.mobileView) {
-      //     return false;
-      //   }
-
-      //   if (settings.show_in_routes !== "") {
-      //     const selectedRoutes = settings.show_in_routes.split("|");
-
-      //     if (
-      //       selectedRoutes.includes(currentRouteName) ||
-      //       selectedRoutes.includes(`c/${categorySlug}`) ||
-      //       selectedRoutes.includes(`tag/${tagId}`)
-      //     ) {
-      //       return true;
-      //     } else {
-      //       return false;
-      //     }
-      //   }
-
-      //   // if theme setting is empty, show everywhere except /categories
-      //   return currentRouteName === "discovery.categories" ? false : true;
-      // }
-
       @discourseComputed(
         "router.currentRouteName",
-        "router.currentRoute.attributes.category"
+        "router.currentRoute.attributes.category",
+        "router.currentRoute.attributes.category.slug",
+        "router.currentRoute.attributes.tag.id"
       )
-      showSidebar(currentRouteName, category) {
+      showSidebar(currentRouteName, category, categorySlug, tagId) {
         if (this.site.mobileView) {
           return false;
         }
@@ -48,19 +22,22 @@ export default {
           let subcategory = null;
           let parentCategory = null;
 
-        // check if current page is subcategory
+          // check if current page is subcategory
           // -- is category
           // -- does not have children itself
           // -- has a parent category
           if (!!this.category &&
             !this.category.has_children &&
             !!this.category.parent_category_id) {
-            subcategory = this.category.slug;
+            subcategory = categorySlug;
             parentCategory = category.ancestors[0].slug
           }
 
           if (
-            selectedRoutes.includes(`"c/${parentCategory}/${subcategory}"`)
+            selectedRoutes.includes(currentRouteName) ||
+            selectedRoutes.includes(`c/${categorySlug}`) ||
+            selectedRoutes.includes(`c/${parentCategory}/${subcategory}`) ||
+            selectedRoutes.includes(`tag/${tagId}`)
           ) {
             return true;
           } else {
@@ -70,7 +47,7 @@ export default {
 
         // if theme setting is empty, show everywhere except /categories
         return currentRouteName === "discovery.categories" ? false : true;
-      },
+      }
     });
   },
 };
